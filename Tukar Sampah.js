5ba91c1dcbe1affae14e9b0f1d188965b90bdee6
njas07 <sutrisnorita5314@gmail.com>
2026-07-11 14:39:51 +0800

    Merapikan halaman Edukasi dan Lokasi serta meningkatkan tampilan website

--- Tukar Sampah.js
+++ Tukar Sampah.js
@@ -241,7 +241,7 @@ function hitungSemuaJarak(){
 
         // Update teks jarak
         card.querySelector(".jarak").innerHTML =
-        `📏 ${jarak.toFixed(2)} km`;
+        ` ${jarak.toFixed(2)} km`;
 
         // Tombol navigasi
         const tombol = card.querySelector(".btn-navigasi");
@@ -310,3 +310,23 @@ function hitungSemuaJarak(){
 }
 
         }
+
+        function hitungJarak(lat1, lon1, lat2, lon2){
+
+    const R = 6371;
+
+    const dLat = (lat2-lat1) * Math.PI / 180;
+    const dLon = (lon2-lon1) * Math.PI / 180;
+
+    const a =
+        Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.cos(lat1*Math.PI/180) *
+        Math.cos(lat2*Math.PI/180) *
+        Math.sin(dLon/2) *
+        Math.sin(dLon/2);
+
+    const c = 2 * Math.atan2(Math.sqrt(a),Math.sqrt(1-a));
+
+    return R*c;
+
+}
